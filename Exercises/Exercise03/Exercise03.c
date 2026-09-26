6e51f162a289eaf8c6d37978231c37f955f5a81f
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-09-26 13:57:12 +0200

    added Exercise03 all

---
+++ Exercises/Exercise03/Exercise03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int height;
+	int base;
+	int area;
+	printf("Enter the height of the triangle:");
+	scanf("%d",&height);
+	printf("Enter the base of the triangle:");
+	scanf("%d",&base);
+	area=(base*height)/2;
+	printf("The area of a triangle is %d",area);
+}
---
+++ Exercises/Exercise03/Readme.md
@@ -0,0 +1,1 @@
+# write a program to find and display the area of a triangle
